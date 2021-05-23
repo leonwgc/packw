0d58268da57a8333fdbf4b8c3dc97c260a42528f
leonwgc <wangguochao@zuifuli.com>
2021-05-23 14:10:45 +0800

    feat :use ts

---
+++ babel.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  presets: [
+    require.resolve('@babel/preset-env', {
+      targets: {
+        node: 'current',
+      },
+    }),
+    require.resolve('@babel/preset-react'),
+    require.resolve('@babel/preset-typescript'),
+  ],
+  plugins: [
+    [
+      require.resolve('@babel/plugin-transform-runtime'),
+      { corejs: { version: 3, proposals: true } },
+    ],
+    [require.resolve('@babel/plugin-proposal-class-properties'), { loose: false }],
+  ],
+};
