16b1390a2007788f3c72077b74e92791b6bb39e0
leonwgc <giantfish@126.com>
2021-09-15 10:51:18 +0800

    chore: update

--- types/lib.d.ts
+++ types/lib.d.ts
@@ -1,2 +1,3 @@
+/** get a ssr commonjs2 lib to render html string */
 export declare const getSsrLib: (entry: any, callback?: () => void) => void;
 export declare const injectHtmlToRootNode: (htmlFilePath: string, html?: string, rootSelector?: string) => void;
