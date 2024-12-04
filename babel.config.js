c3efacb603b3a8998983c531663e32a4c3ab5fe1
leonwgc <leon.wang@derbysoft.net>
2024-12-04 17:48:53 +0800

    feat: 2.3.3

--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,12 @@
 module.exports = {
-  presets: [require.resolve('@babel/preset-env'), require.resolve('@babel/preset-typescript')],
+  presets: [
+    [require.resolve('@babel/preset-env'), { loose: true }],
+    require.resolve('@babel/preset-typescript'),
+  ],
   plugins: [
     [
       require.resolve('@babel/plugin-transform-runtime'),
       { corejs: { version: 3, proposals: true } },
     ],
-    ['@babel/plugin-proposal-private-property-in-object', { loose: true }],
-    ['@babel/plugin-proposal-private-methods', { loose: true }],
   ],
 };
